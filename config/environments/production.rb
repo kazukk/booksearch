2ff0e92493cd388c73855cbc20b584f64e4d4d34
kazukk <kazuhiro.shimizu@gmail.com>
2015-07-06 16:01:48 +0900

    confit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,13 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+
+######## # Disable Rails's static asset server (Apache or nginx will already do this).
+  config.serve_static_assets = true
+
+
+
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
